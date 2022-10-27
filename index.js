9cbe7664ab1c82e23f78f0b23be0aaffeaf01ea8
Uros Grujicic <uros.grujicic@digitalatrium.rs>
2022-10-27 10:36:32 +0200

    Connecting to the database

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const morgan = require('morgan');
+const connectDB = require('./db');
 
 dotenv.config({ path: './config.env'});
+
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-console.log(PORT);
-
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
